d343f44d0daf0427766332f0f96e23a56390f4c1
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-06-15 17:28:22 -0400

    Added escavalier

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,14 @@ var pokemonList = {
 		height: 2,
 		types: ['grass','poison']
 	}
-var zapdos = {
+	var zapdos = {
 		name: 'Zapdos', 
 		height: 2,
 		types: ['electric','flying']
 	}
+	var escavalier = {
+		name: 'Escavalier'
+		height: 1,
+		types: ['steel','bug']
+	} 
 }
