645967a0420289bfdee4ac083499fef83abc82df
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-06-17 10:24:48 -0400

    Fixed indentation issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 var pokemonList = [
 {
-		name: 'Venusaur',
-		height: 2,
-		types: ['grass','poison'],
+	name: 'Venusaur',
+	height: 2,
+	types: ['grass','poison'],
 	},
 {
-		name: 'Volcarona',
-		height: 1.6,
-		types: ['fire','bug'],
+	name: 'Volcarona',
+	height: 1.6,
+	types: ['fire','bug'],
 	},
 {
-		name: 'Escavalier',
-		height: 1,
-		types: ['steel','bug'],
+	name: 'Escavalier',
+	height: 1,
+	types: ['steel','bug'],
 	},
 ];
+
 for ( var i = 0; i < pokemonList.length; i++) {
   document.write('<p>' + pokemonList[i].name  + ' \(height: ' + pokemonList[i].height + '\)')
 	if (pokemonList[i].height >= 2) {
