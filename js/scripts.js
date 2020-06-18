23d89ba9fc18c556127210800eefc7b929b41a1b
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-06-18 14:06:39 -0400

    Changed tabs to spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,40 @@
 var pokemonRepository = (function () {
-	var pokemonList = [
+  var pokemonList = [
 {
-	name: 'Venusaur',
-	height: 2,
-	types: ['grass','poison'],
-	},
+  name: 'Venusaur',
+  height: 2,
+  types: ['grass','poison'],
+  },
 {
-	name: 'Volcarona',
-	height: 1.6,
-	types: ['fire','bug'],
-	},
+  name: 'Volcarona',
+  height: 1.6,
+  types: ['fire','bug'],
+  },
 {
-	name: 'Escavalier',
-	height: 1,
-	types: ['steel','bug'],
+  name: 'Escavalier',
+  height: 1,
+  types: ['steel','bug'],
 }
 ];
 
 function add(pokemon) {
-	if (typeof pokemon === 'object');
-		pokemonList.push(pokemon);
+  if (typeof pokemon === 'object');
+    pokemonList.push(pokemon);
 }
 
 function getAll(pokemon) {
-	return pokemonList;
+  return pokemonList;
 }
 
 return {
-	add: add,
-	getAll: getAll,
+  add: add,
+  getAll: getAll,
 }
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon){
-	document.write('<p>' + pokemon.name + ' \(height: ' + pokemon.height + '\)')
-	if (pokemon.height >= 2) {
-		document.write('- Wow, that\'s big!' + '<p>' )
-	}
+  document.write('<p>' + pokemon.name + ' \(height: ' + pokemon.height + '\)')
+  if (pokemon.height >= 2) {
+    document.write('- Wow, that\'s big!' + '<p>' )
+  }
 });
