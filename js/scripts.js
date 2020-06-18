2a396e3347de89e6d4aea3bf928e086a8bed4321
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-06-18 13:06:09 -0400

    Wrap array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var pokemonList = [
+var pokemonRepository = (function () {
+	var pokemonList = [
 {
 	name: 'Venusaur',
 	height: 2,
@@ -13,14 +14,26 @@ var pokemonList = [
 	name: 'Escavalier',
 	height: 1,
 	types: ['steel','bug'],
-	},
+}
 ];
 
-function myLoopFunction(pokemon) {
+function add(pokemon) {
+	pokemonList.push(pokemon);
+}
+
+function getAll(pokemon) {
+	return pokemonList;
+}
+
+return {
+	add: add,
+	getAll: getAll,
+}
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
 	document.write('<p>' + pokemon.name + ' \(height: ' + pokemon.height + '\)')
 	if (pokemon.height >= 2) {
 		document.write('- Wow, that\'s big!' + '<p>' )
 	}
-}
-pokemonList.forEach(myLoopFunction);
-
+});
