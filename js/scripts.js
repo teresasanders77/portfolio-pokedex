8038756ba907af7468021aebbbb792e75c8227ed
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-06-22 15:44:53 -0400

    Added buttons for each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,48 @@
-var pokemonList = [
+var pokemonRepository = (function () {
+  var pokemonList = [
 {
-		name: 'Venusaur',
-		height: 2,
-		types: ['grass','poison'],
-	},
+  name: 'Venusaur',
+  height: 2,
+  types: ['grass','poison'],
+  },
 {
-		name: 'Volcarona',
-		height: 1.6,
-		types: ['fire','bug'],
-	},
+  name: 'Volcarona',
+  height: 1.6,
+  types: ['fire','bug'],
+  },
 {
-		name: 'Escavalier',
-		height: 1,
-		types: ['steel','bug'],
-	},
-];
-for ( var i = 0; i < pokemonList.length; i++) {
-  document.write('<p>' + pokemonList[i].name  + ' \(height: ' + pokemonList[i].height + '\)')
-	if (pokemonList[i].height >= 2) {
-		document.write('- Wow, that\'s big!' + '<p>' )
-	}
+  name: 'Escavalier',
+  height: 1,
+  types: ['steel','bug'],
 }
+];
+
+function add(pokemon) {
+  if (typeof pokemon !== 'object') {
+    return 'Not a valid input'
+  }else{
+    pokemonList.push(pokemon);
+  }
+}
+
+function getAll(pokemon) {
+  return pokemonList;
+}
+
+return {
+  add: add,
+  getAll: getAll,
+}
+})();
+
+var $pokemonList = document.querySelector('ul');
+pokemonRepository.getAll().forEach(function(pokemon){
+  var $listItem = document.createElement('li');
+  $pokemonList.appendChild($listItem)
+  var button = document.createElement('button');
+  $listItem.appendChild(button);
+  button.innerText = pokemon.name;
+  button.classList.add('list-button');
+  $listItem.classList.add('button');
+
+});
