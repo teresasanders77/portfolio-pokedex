b932d887537bedef593905db0a2e5d62263ab8d1
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-06-23 12:26:22 -0400

    added event listener and comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//wrapped in IIFE to avoid acessing the global state
 var pokemonRepository = (function () {
   var pokemonList = [
 {
@@ -14,9 +15,10 @@ var pokemonRepository = (function () {
   name: 'Escavalier',
   height: 1,
   types: ['steel','bug'],
-}
+  }
 ];
 
+//adds pokemon to pokemonList as long as it's an object
 function add(pokemon) {
   if (typeof pokemon !== 'object') {
     return 'Not a valid input'
@@ -29,20 +31,35 @@ function getAll(pokemon) {
   return pokemonList;
 }
 
+function addListItem(pokemon) {
+var $listItem = document.createElement('li');
+  $pokemonList.appendChild($listItem)
+//adds button for each list item
+var button = document.createElement('button');
+  $listItem.appendChild(button);
+//button inner text
+button.innerText = pokemon.name;
+button.classList.add('list-button');
+$listItem.classList.add('button');
+//logs details of each pokemon when clicked
+button.addEventListener('click', function () {
+  showDetails(pokemon)
+  })
+}
+
+function showDetails(pokemon) {
+  console.log(pokemon);
+}
+
 return {
   add: add,
   getAll: getAll,
-}
+  addListItem: addListItem,
+  }
 })();
 
+//for each loop iterates over the pokemonList; prints details of each
 var $pokemonList = document.querySelector('ul');
 pokemonRepository.getAll().forEach(function(pokemon){
-  var $listItem = document.createElement('li');
-  $pokemonList.appendChild($listItem)
-  var button = document.createElement('button');
-  $listItem.appendChild(button);
-  button.innerText = pokemon.name;
-  button.classList.add('list-button');
-  $listItem.classList.add('button');
-
+  pokemonRepository.addListItem(pokemon);
 });
